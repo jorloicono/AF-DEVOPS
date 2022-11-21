49eabaaed05028e4dd4022dbd6ba36f9db8c68ed
Jorge López Blasco <115549197+jorloicono@users.noreply.github.com>
2022-11-21 13:15:07 +0100

    Update calculadora_integration_test.go

--- PRUEBAS/calculadora_integration_test.go
+++ PRUEBAS/calculadora_integration_test.go
@@ -1,20 +1,23 @@
-package main
 
+// +build integration
+ 
+package main
+ 
 import "testing"
 import "fmt"
-
+            
 func TestIntegrationCalculadora(t *testing.T) {
-	suma := Suma(8, 8)
-	resta := Resta(8, 8)
-	multiplicacion := Multiplicacion(8, 8)
-	division = Division(8, 8)
-	promedio = Promedio(8, 8, 8, 8, 8)
-
-	promediototal := Promedio(suma, resta, multiplicacion, division, promedio)
-
-	if promediototal != 17 {
-		t.Error("Se esperaba un 17 y se ha obtenido ", promediototal)
-	}
-
-	fmt.Println("Prueba de integracion exitosa")
+            suma := Suma(8, 8)
+            resta := Resta(8, 8)     
+            multiplicacion := Multiplicacion(8, 8)
+            division := Division(8, 8)
+            promedio := Promedio(8, 8, 8, 8, 8)
+ 
+            promediototal := Promedio(suma, resta, multiplicacion, division, promedio)
+ 
+            if promediototal != 17 {
+                        t.Error("Se esperaba 17 y se obtuvo", promediototal)
+            }
+            
+            fmt.Println("Prueba de integracion del promedio total fue exitosa")
 }
